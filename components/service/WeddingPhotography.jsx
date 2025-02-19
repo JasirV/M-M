a7bf46a5116d9d7471931040efcef7516feb5c43
JasirV <jasirmon133@gmail.com>
2025-02-19 19:14:19 +0530

    updateserverivPage

--- components/service/WeddingPhotography.jsx
+++ components/service/WeddingPhotography.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 
 const WeddingPhotography = ({ heading, img1, img2, img3, description, index }) => {
   return (
-    <div className={`w-full flex flex-col md:flex-row mt-10 items-center py-10 gap-10 justify-center p-4 px-6 md:px-20 ${index % 2 === 0 ? ' ' : 'md:flex-row-reverse '}`}>
+    <div className={`w-full flex flex-col md:flex-row mt-10 items-center py-10 gap-10 justify-center p-4 px-6 md:px-20 ${index % 2 === 0 ? ' ' : 'md:flex-row-reverse text-white'}`}
+    style={{
+      backgroundImage: index % 2 !== 0 ? 'url("https://i.pinimg.com/originals/af/c8/cc/afc8cc99d1636140609ca586a4b67f9b.jpg")' : 'none',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}>
       
       {/* Heading */}
       <div className="w-full md:w-1/3 text-center md:text-left">
