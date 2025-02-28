032ddc4be5a7567080c189f44260deff54e625b7
JasirV <jasirmon133@gmail.com>
2025-02-28 21:37:38 +0530

    updatecontactPage

--- components/contact/Information.jsx
+++ components/contact/Information.jsx
@@ -2,7 +2,7 @@ import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 const Information = () => {
   return (
-    <div className=" flex flex-col items-center justify-center p-8 bg-white text-black">
+    <div className=" flex flex-col items-center justify-center p-8 ">
       <h1 className="text-4xl font-normal tracking-wide font-garamond uppercase  pb-2 mb-12">
         Contact Information
       </h1>
@@ -10,29 +10,29 @@ const Information = () => {
       <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-4 gap-8">
         
         {/* Head Office */}
-        <div className="p-6 border border-black rounded-lg shadow-md flex flex-col items-center text-center">
-          <FaMapMarkerAlt className="text-black text-4xl mb-4" />
+        <div className="p-6 border border-black dark:border-white rounded-lg shadow-md flex flex-col items-center text-center">
+          <FaMapMarkerAlt className="text-black dark:text-white text-4xl mb-4" />
           <h2 className="text-xl font-normal uppercase font-poppins">Head Office</h2>
           <p className="text-gray-700 font-poppins">Kochi, Kerala</p>
         </div>
 
         {/* Branch Office */}
-        <div className="p-6 border border-black rounded-lg shadow-md flex flex-col items-center text-center">
-          <FaMapMarkerAlt className="text-black text-4xl mb-4" />
+        <div className="p-6 border border-black dark:border-white rounded-lg shadow-md flex flex-col items-center text-center">
+          <FaMapMarkerAlt className="text-black dark:text-white text-4xl mb-4" />
           <h2 className="text-xl font-normal uppercase font-poppins">Branch Office</h2>
           <p className="text-gray-700 font-poppins">Kuppooth, Palakkad, Kerala</p>
         </div>
 
         {/* Phone */}
-        <div className="p-6 border border-black rounded-lg shadow-md flex flex-col items-center text-center">
-          <FaPhoneAlt className="text-black text-4xl mb-4" />
+        <div className="p-6 border border-black dark:border-white rounded-lg shadow-md flex flex-col items-center text-center">
+          <FaPhoneAlt className="text-black dark:text-white text-4xl mb-4" />
           <h2 className="text-xl font-normal uppercase font-poppins">Phone</h2>
           <p className="text-gray-700 font-poppins">976968968</p>
         </div>
 
         {/* Email */}
-        <div className="p-6 border border-black rounded-lg shadow-md flex flex-col items-center text-center">
-          <FaEnvelope className="text-black text-4xl mb-4" />
+        <div className="p-6 border border-black dark:border-white rounded-lg shadow-md flex flex-col items-center text-center">
+          <FaEnvelope className="text-black dark:text-white text-4xl mb-4" />
           <h2 className="text-xl font-normal uppercase font-poppins">Email</h2>
           <p className="text-gray-700 font-poppins">mmweddin@gmail.com</p>
         </div>
--- components/contact/SocialMedia.jsx
+++ components/contact/SocialMedia.jsx
@@ -5,22 +5,22 @@ const SocialMedia = () => {
   return (
     <div className='flex justify-center p-5 '>
         <div className=' flex md:gap-20 p-3 flex-wrap justify-center gap-5'>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaFacebookF size={30} />
             </div>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaInstagram size={30}/>
             </div>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaYoutube size={30}/>
             </div>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaPinterest size={30}/>
             </div>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaTwitter size={30}/>
             </div>
-            <div className='p-4 hover:bg-gray-300 rounded-full'>
+            <div className='p-4 hover:bg-gray-300 dark:hover:bg-gray-900 rounded-full'>
         <FaLinkedinIn size={30}/>
             </div>
         </div>
